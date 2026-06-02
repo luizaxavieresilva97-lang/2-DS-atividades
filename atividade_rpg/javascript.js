96a59a16022ba803f44450ecdd63fe7a9bde5af3
luiza <luizaxavieresilva97@gmail.com>
2026-06-02 12:17:14 -0300

    atividade

--- atividade_rpg/javascript.js
+++ atividade_rpg/javascript.js
@@ -6,7 +6,31 @@ class Personagem {
         this.mana = mana;
         this.energia = energia;
     }
-
+    hero_atacar(alvo, habilidade) {
+        if (this.mana >= habilidade.custo 
+            && this.energia >= habilidade.energia) {
+                alvo.hp = alvo.hp - habilidade.dano;
+                // Debitar Mana
+                if (habilidade.custo > 0) {
+                    this.mana -= habilidade.custo;
+                    this.energia += 50;
+                }
+                // Debitar Energia
+                if (habilidade.energia > 0) {
+                    this.energia = 0;
+                }
+            } else {
+                return "Sem mana ou energia.";
+            }
+    }
+    boss_atacar(alvo) {
+        if(this.energia >= 100) {
+            alvo.hp -= 15;
+            this.energia = 0;
+        } else {
+            this.energia += 50;
+        }
+    }
 }
 class Habilidade{
     constructor(id, nome, dano, custo, energia){
@@ -28,7 +52,8 @@ document.getElementById("titulo-heroi").textContent = hero.titulo;
 document.getElementById("nome-boss").textContent = boss.nome;
 document.getElementById("titulo-boss").textContent = boss.titulo;
 //criar habilidades
-let listhabilidades = [
+let containerBtn = document.getElementById("controles");
+let listahabilidades = [
     new Habilidade(1, "⚔️ Ataque", 4, 0, 0),
     new Habilidade(2, "🪙 skill", 8, 10, 0),
     new Habilidade(1, "💥 supremo", 15, 0, 100),
@@ -39,6 +64,13 @@ listahabilidades.forEach(Hab => {
     btn.classList.add("btn","btn-outline-warning");
     containerBtn.appendChild(btn);
     btn.onclick = () => {
-        hero.hero_atacar(boss, hab)
+        hero.hero_atacar(boss, Hab);
+          boss.boss_atacar(hero);
+        atualizarTela();
     }
-});
+}) ;
+const atualizarTela = () => {
+    document.getElementById("hp-boss").value = boss.hp;
+    document.getElementById("mp-hero").value = hero.mana;
+    document.getElementById("en-hero").value = hero.energia;
+}
